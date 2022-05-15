850b9f0227dc33cef6fa20bcccb231bb372ee20d
Kyuofox <88403766@qq.com>
2022-05-15 10:55:18 +0800

    techpack: camera: Import xiaomi dxo changes
    Signed-off-by: Kyuofox <88403766@qq.com>

--- techpack/camera/drivers/cam_isp/isp_hw_mgr/isp_hw/vfe_hw/cam_vfe_soc.c
+++ techpack/camera/drivers/cam_isp/isp_hw_mgr/isp_hw/vfe_hw/cam_vfe_soc.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2017-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2017-2019, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2021 XiaoMi, Inc.
  */
 
 #include <linux/slab.h>
@@ -84,9 +85,6 @@ static int cam_vfe_get_dt_properties(struct cam_hw_soc_info *soc_info)
 	default:
 		break;
 	}
-
-	vfe_soc_private->dsp_disabled = of_property_read_bool(of_node,
-		"dsp-disabled");
 
 end:
 	return rc;
