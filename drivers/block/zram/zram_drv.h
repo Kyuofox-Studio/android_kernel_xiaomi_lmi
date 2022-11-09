d71547bc0ecf1ecafa1a28627b09c0dd4c19cea1
Sergey Senozhatsky <senozhatsky@chromium.org>
2022-11-09 20:50:38 +0900

    zram: introduce recompress sysfs knob
    Allow zram to recompress (using secondary compression streams)
    pages.
    
    Re-compression algorithms (we support up to 3 at this stage)
    are selected via recomp_algorithm:
    
      echo "algo=zstd priority=1" > /sys/block/zramX/recomp_algorithm
    
    Please read documentation for more details.
    
    We support several recompression modes:
    
    1) IDLE pages recompression is activated by `idle` mode
    
      echo "type=idle" > /sys/block/zram0/recompress
    
    2) Since there may be many idle pages user-space may pass a size
    threshold value (in bytes) and we will recompress pages only
    of equal or greater size:
    
      echo "threshold=888" > /sys/block/zram0/recompress
    
    3) HUGE pages recompression is activated by `huge` mode
    
      echo "type=huge" > /sys/block/zram0/recompress
    
    4) HUGE_IDLE pages recompression is activated by `huge_idle` mode
    
      echo "type=huge_idle" > /sys/block/zram0/recompress
    
    [senozhatsky@chromium.org: we should always zero out err variable in recompress loop[
      Link: https://lkml.kernel.org/r/20221110143423.3250790-1-senozhatsky@chromium.org
    Link: https://lkml.kernel.org/r/20221109115047.2921851-5-senozhatsky@chromium.org
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Cc: Nathan Chancellor <nathan@kernel.org>
    Cc: Alexey Romanov <avromanov@sberdevices.ru>
    Cc: Nhat Pham <nphamcs@gmail.com>
    Cc: Nitin Gupta <ngupta@vflare.org>
    Cc: Suleiman Souhlal <suleiman@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -43,6 +43,9 @@
  */
 #define ZRAM_FLAG_SHIFT 24
 
+/* Only 2 bits are allowed for comp priority index */
+#define ZRAM_COMP_PRIORITY_MASK	0x3
+
 /* Flags for zram pages (table[page_no].flags) */
 enum zram_pageflags {
 	/* zram slot is locked */
@@ -52,6 +55,10 @@ enum zram_pageflags {
 	ZRAM_UNDER_WB,	/* page is under writeback */
 	ZRAM_IDLE,	/* not accessed page since last idle marking */
 	ZRAM_DEDUPED,	/* Deduplicated with existing entry */
+	ZRAM_INCOMPRESSIBLE, /* none of the algorithms could compress it */
+
+	ZRAM_COMP_PRIORITY_BIT1, /* First bit of comp priority index */
+	ZRAM_COMP_PRIORITY_BIT2, /* Second bit of comp priority index */
 
 	__NR_ZRAM_PAGEFLAGS,
 };
@@ -64,6 +71,8 @@ struct zram_entry {
 	u64 checksum;
 	unsigned long refcount;
 	unsigned long handle;
+	unsigned long handle_old;
+	unsigned long handle_new;
 };
 
 /* Allocated for each disk page */
